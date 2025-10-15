dc5a71142e88f67250884c5db78cd4655c389b59
mahirnav <mahirnav@gmail.com>
2025-10-15 18:21:51 -0400

    Animation Lerping for locomotion

--- Assets/MonkeyStudios/FinalCharacterController/Scripts/PlayerAnimation.cs
+++ Assets/MonkeyStudios/FinalCharacterController/Scripts/PlayerAnimation.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private float locomotionBlendSpeed = 0.02f;
 
     private PlayerLocomotionInput _playerLocomotionInput;
 
     private static int inputXHash = Animator.StringToHash("inputX");
     private static int inputYHash = Animator.StringToHash("inputY");
+
+    private Vector3 _currentBlendInput = Vector3.zero;
 
     private void Awake()
     {
@@ -22,8 +25,9 @@ public class PlayerAnimation : MonoBehaviour
     private void UpdateAnimationState()
     {
         Vector2 inputTarget = _playerLocomotionInput.MovementInput;
+        _currentBlendInput = Vector3.Lerp(_currentBlendInput, inputTarget, locomotionBlendSpeed * Time.deltaTime);
 
-        _animator.SetFloat(inputXHash, inputTarget.x);
-        _animator.SetFloat(inputYHash, inputTarget.y);
+        _animator.SetFloat(inputXHash, _currentBlendInput.x);
+        _animator.SetFloat(inputYHash, _currentBlendInput.y);
     }
 }
